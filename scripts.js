5e3e9c44d7c79fdc558e3625f9fffc08a9e7649e
nabmctackle <nabmctackle@gmail.com>
2022-01-13 18:40:10 -0800

    first committ

---
+++ scripts.js
@@ -0,0 +1,11 @@
+/**
+
+* Filename: scripts.js
+* Author: Christopher J. Anicello
+* Description: This page is my introduction and demonstrates
+*     my understanding of HTML and CSS.
+
+Wasnt sure how in depth the webpage is supposed to be. I didnt see a need for any javascript based on the requirements 
+but let me know if there was something you expected that was missing.
+
+*/
